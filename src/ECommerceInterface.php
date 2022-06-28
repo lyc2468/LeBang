54a2b8f6a47644e0ec264ff3320a8c752db6b276
lyc2468 <lyc2468@users.noreply.github.com>
2022-06-28 13:08:00 +0000

    Fix styling

--- src/ECommerceInterface.php
+++ src/ECommerceInterface.php
@@ -5,5 +5,6 @@ namespace Skies\LeBang;
 interface ECommerceInterface
 {
     public static function detail(ApiClient $client, string $url, array $params = [], bool $isFormat = false): array|StandardFormat|null;
+
     public static function detect(string $url): bool;
 }
